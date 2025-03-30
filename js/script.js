81067b89907555ae2e859d8e3b183ce9a6a28d2a
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-30 23:36:40 +0200

    [UPD] DEFINIZIONE DELLE VARIABILI

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // DEFINIZIONE DELLE VARIABILI, DELLE FUNZIONI E/O RECUPERO DEGLI ELEMENTI DEL DOM
 // RECUPERO GLI ELEMENTI DEL DOM CHE MI SERVONO
-const form = document.getElementById(`ansewer-form`);
+const form = document.getElementById(`answers-form`);
 const inputFields = document.querySelectorAll(`input`);
+const countdown = document.getElementById(`countdown`);
+const numberList = document.getElementById(`numbers-list`);
+const message = document.getElementById(`message`);
+const instruction = document.getElementById(`instruction`);
+const button = document.querySelector(`button`);
+
+// DEFINIZIONE DELLE VARIABILI
+const min = 1;
+const max = 50;
+const totalNumbers = 5;
+const time = 30;
+
+
+
 
